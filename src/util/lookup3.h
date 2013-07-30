5c67f8d15c4cc4a65833813b470429f5167c5954
Phil Carns <carns@mcs.anl.gov>
2013-07-30 16:04:19 -0400

    remove unused versions of jenkins hash fns

--- src/util/lookup3.h
+++
@@ -1,9 +0,0 @@
-#ifndef SRC_COMMON_UTIL_LOOKUP3_H
-#define SRC_COMMON_UTIL_LOOKUP3_H
-
-uint32_t hashword(const uint32_t *k, size_t length, uint32_t initval);
-uint32_t hashlittle(const void * key, size_t length, uint32_t initval);
-void hashlittle2(const void * key, size_t length, uint32_t * pc, uint32_t * pb);
-uint32_t hashbig(const void * key, size_t length, uint32_t initval);
-
-#endif
