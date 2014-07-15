239d62f02cc74e576c9429bc41366a8d0ea0b41d
John Jenkins <jenkins@mcs.anl.gov>
2014-07-15 14:29:11 -0500

    fixes to make dumpi completely optional

--- src/network-workload/codes-nw-test.c
+++ src/network-workload/codes-nw-test.c
@@ -6,6 +6,7 @@
 #include "codes/codes-nw-workload.h"
 #include "codes/codes.h"
 
+char workload_type[128];
 char workload_file[8192];
 char offset_file[8192];
 static int total_nw_lps = 8;
@@ -36,11 +37,29 @@ tw_peid nw_test_map(tw_lpid gid)
 void nw_test_init(nw_state* s, tw_lp* lp)
 {
    /* initialize the LP's and load the data */
-   //scala_trace_params params;
-   //strcpy(params.offset_file_name, offset_file);
-   //strcpy(params.nw_wrkld_file_name, workload_file);
-   dumpi_trace_params params;
-   strcpy(params.file_name, workload_file);
+   char * params;
+   scala_trace_params params_sc;
+#if USE_DUMPI
+   dumpi_trace_params params_d;
+#endif
+   if (strcmp(workload_type, "scalatrace") == 0){
+       if (params_sc.offset_file_name[0] == '\0'){
+           tw_error(TW_LOC, "required argument for scalatrace offset_file");
+           return;
+       }
+       strcpy(params_sc.offset_file_name, offset_file);
+       strcpy(params_sc.nw_wrkld_file_name, workload_file);
+       params = (char*)&params_sc;
+   }
+   else if (strcmp(workload_type, "dumpi") == 0){
+#if USE_DUMPI
+       strcpy(params_d.file_name, workload_file);
+       params = (char*)&params_d;
+#else
+       tw_error(TW_LOC, "dumpi support not enable");
+       return;
+#endif
+   }
    wrkld_id = codes_nw_workload_load("dumpi-trace-workload", (char*)&params, (int)lp->gid);
    
    tw_event *e;
@@ -84,6 +103,7 @@ void nw_test_event_handler_rc(nw_state* s, tw_bf * bf, nw_message * m, tw_lp * l
 const tw_optdef app_opt [] =
 {
 	TWOPT_GROUP("Network workload test"),
+    TWOPT_CHAR("workload_type", workload_type, "workload type (either \"scalatrace\" or \"dumpi\")"),
 	TWOPT_CHAR("workload_file", workload_file, "workload file name"),
 	TWOPT_CHAR("offset_file", offset_file, "offset file name"),
 	TWOPT_UINT("total_nw_lps", total_nw_lps, "total number of LPs"),
@@ -107,13 +127,14 @@ int main( int argc, char** argv )
   int 	i;
   char log[32];
 
+  workload_type[0]='\0';
   tw_opt_add(app_opt);
   tw_init(&argc, &argv);
 
   if(strlen(workload_file) == 0 || total_nw_lps == 0)
     {
 	if(tw_ismaster())
-		printf("\n Usage: mpirun -np n ./codes-nw-test --sync=1/2/3 --total_nw_lps=n --workload_file=workload-file-name");
+		printf("\n Usage: mpirun -np n ./codes-nw-test --sync=1/2/3 --total_nw_lps=n --workload_type=type --workload_file=workload-file-name");
 	tw_end();
 	return -1;
     }
