80353138e92ab5ea5ca4364e626415513682fb36
mubarak <mubarak@mcs.anl.gov>
2016-12-09 11:47:54 -0500

    Fixing custom dragonfly test program

--- tests/modelnet-test-dragonfly-custom-synthetic.sh
+++ tests/modelnet-test-dragonfly-custom-synthetic.sh
@@ -5,7 +5,7 @@ if [ -z $srcdir ]; then
               exit 1
  fi
 
-src/network-workloads/model-net-synthetic-custom-dfly --sync=1 --num_messages=1 -- $srcdir/src/network-workloads/conf/dragonfly-custom/modelnet-test-dragonfly-theta.conf 
+src/network-workloads/model-net-synthetic-custom-dfly --sync=1 --num_messages=1 -- src/network-workloads/conf/dragonfly-custom/modelnet-test-dragonfly-theta.conf 
 
 
 
