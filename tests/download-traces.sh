753bb7a83d9b604809bcd64ba0a25a38d51d4158
mubarak <mubarak@mcs.anl.gov>
2016-12-14 12:19:16 -0500

    reverting path changes

--- tests/download-traces.sh
+++ tests/download-traces.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-FILE=df_AMG_n27_dumpi/dumpi-2014.03.03.14.55.00-0000.bin
+FILE=/tmp/df_AMG_n27_dumpi/dumpi-2014.03.03.14.55.00-0000.bin
 
 if [ ! -f $FILE ]; then
        wget https://portal.nersc.gov/project/CAL/doe-miniapps-mpi-traces/AMG/df_AMG_n27_dumpi.tar.gz
-       tar -xvf df_AMG_n27_dumpi.tar.gz
+       tar -xvf df_AMG_n27_dumpi.tar.gz -C /tmp
 fi
