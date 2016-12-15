a133054b0e23ecde9d0d977749bcd9260adb47b1
mubarak <mubarak@mcs.anl.gov>
2016-12-14 19:24:33 -0500

    Enabling optimistic mode for DUMPI trace tests

--- tests/modelnet-test-dragonfly-synthetic.sh
+++ tests/modelnet-test-dragonfly-synthetic.sh
@@ -9,6 +9,6 @@ source $srcdir/tests/download-traces.sh
 
 src/network-workloads/model-net-synthetic --sync=1 --num_messages=1 -- $srcdir/src/network-workloads/conf/modelnet-synthetic-dragonfly.conf 
 
-src/network-workloads/model-net-mpi-replay --sync=1 --num_net_traces=27 --workload_file=/tmp/df_AMG_n27_dumpi/dumpi-2014.03.03.14.55.00- --workload_type="dumpi" -- $srcdir/src/network-workloads/conf/modelnet-mpi-test-dfly-amg-216.conf 
+mpirun -np 2 src/network-workloads/model-net-mpi-replay --sync=3 --num_net_traces=27 --workload_file=/tmp/df_AMG_n27_dumpi/dumpi-2014.03.03.14.55.00- --workload_type="dumpi" -- $srcdir/src/network-workloads/conf/modelnet-mpi-test-dfly-amg-216.conf 
 
 
