codes-org/codes

commit 77e85ef978ec40a17d239e0ab52b222e075fee09

changes related to the ROSS model-level sampling feature

src/networks/model-net/fattree.c
@@ -1811,6 +1811,7 @@ void switch_packet_receive_rc(switch_state * s,
 	s_arrive_r++;
 #endif
     int output_port = msg->saved_vc;
+    tw_rand_reverse_unif(lp->rng);
     if(bf->c1)
     {
         tw_rand_reverse_unif(lp->rng);
@@ -2636,10 +2637,13 @@ int ft_get_output_port( switch_state * s, tw_bf * bf, fattree_message * msg,
 
   assert(end_port > start_port);
 
-  outport = start_port;
+  //outport = start_port;
+  // when occupancy is same, just choose random port
+  outport = tw_rand_integer(lp->rng, start_port, end_port-1);  
   int load = s->vc_occupancy[outport] + s->queued_length[outport];
   if(load != 0) {
-    for(int port = start_port + 1; port < end_port; port++) {
+    //for(int port = start_port + 1; port < end_port; port++) {
+    for(int port = start_port; port < end_port; port++) {
       if(s->vc_occupancy[port] +  s->queued_length[port] < load) {
         load = s->vc_occupancy[port] +  s->queued_length[port];
         outport = port;
@@ -2968,23 +2972,27 @@ void fattree_event_collect(fattree_message *m, tw_lp *lp, char *buffer, int *col
     memcpy(buffer, &type, sizeof(type));
 }
 
-st_trace_type fattree_trace_types[] = {
+// TODO will need to separate fattree_method into one for terminal and one for switch
+// in order to use the ROSS model stats collection
+st_model_types fattree_model_types[] = {
     {(rbev_trace_f) fattree_event_collect,
      sizeof(int),
      (ev_trace_f) fattree_event_collect,
-     sizeof(int)},
+     sizeof(int),
+    NULL,
+    0},
     {0}
 };
 
-static const st_trace_type  *fattree_get_trace_types(void)
+static const st_model_types  *fattree_get_model_stat_types(void)
 {
-    return(&fattree_trace_types[0]);
+    return(&fattree_model_types[0]);
 }
 
-static void fattree_register_trace(st_trace_type *base_type)
+static void fattree_register_model_stats(st_model_types *base_type)
 {
-    trace_type_register(LP_CONFIG_NM, base_type);
-    trace_type_register("fattree_switch", &fattree_trace_types[0]);
+    st_model_type_register(LP_CONFIG_NM, base_type);
+    st_model_type_register("fattree_switch", &fattree_model_types[0]);
     //trace_type_register("fattree_switch", base_type);
 }
 /*** END of ROSS event tracing additions */
@@ -3018,7 +3026,7 @@ struct model_net_method fattree_method =
 //  .model_net_method_find_local_device = NULL,
   .mn_collective_call = NULL,
   .mn_collective_call_rc = NULL,
-  .mn_trace_register = fattree_register_trace,
-  .mn_get_trace_type = fattree_get_trace_types
+  .mn_model_stat_register = fattree_register_model_stats,
+  .mn_get_model_stat_types = fattree_get_model_stat_types
 };
 

codes/lp-type-lookup.h
@@ -21,8 +21,8 @@ const tw_lptype* lp_type_lookup(const char* name);
 /* register an LP with CODES/ROSS */
 void lp_type_register(const char* name, const tw_lptype* type);
 
-void trace_type_register(const char* name, const st_trace_type* type);
-const st_trace_type* trace_type_lookup(const char* name);
+void st_model_type_register(const char* name, const st_model_types* type);
+const st_model_types* st_model_type_lookup(const char* name);
 #ifdef __cplusplus
 }
 #endif

codes/model-net-method.h
@@ -63,8 +63,8 @@ struct model_net_method
     revent_f mn_sample_rc_fn;
     init_f mn_sample_init_fn;
     final_f mn_sample_fini_fn;
-    void (*mn_trace_register)(st_trace_type *base_type);
-    const st_trace_type* (*mn_get_trace_type)();
+    void (*mn_model_stat_register)(st_model_types *base_type);
+    const st_model_types* (*mn_get_model_stat_types)();
 };
 
 extern struct model_net_method * method_array[];

codes/model-net.h
@@ -388,7 +388,7 @@ void model_net_set_msg_param(
 
 /* returns pointer to LP information for simplenet module */
 const tw_lptype* model_net_get_lp_type(int net_id);
-const st_trace_type* model_net_get_trace_type(int net_id);
+const st_model_types* model_net_get_model_stat_type(int net_id);
 
 DEPRECATED
 uint64_t model_net_get_packet_size(int net_id);

codes/net/dragonfly.h
@@ -91,6 +91,8 @@ struct terminal_message
    tw_stime saved_hist_start_time;
    tw_stime saved_sample_time;
    tw_stime msg_start_time;
+   tw_stime saved_busy_time_ross;
+   tw_stime saved_fin_chunks_ross;
 
    int saved_hist_num;
    int saved_occupancy;

src/network-workloads/model-net-mpi-replay.c
@@ -1569,22 +1569,29 @@ void nw_lp_event_collect(nw_message *m, tw_lp *lp, char *buffer, int *collect_fl
     memcpy(buffer, &type, sizeof(type));
 }
 
-st_trace_type nw_lp_trace_types[] = {
+void nw_lp_model_stat_collect(nw_state *s, tw_lp *lp, char *buffer)
+{
+    return;
+}
+
+st_model_types nw_lp_model_types[] = {
     {(rbev_trace_f) nw_lp_event_collect,
      sizeof(int),
      (ev_trace_f) nw_lp_event_collect,
-     sizeof(int)},
+     sizeof(int),
+     (model_stat_f) nw_lp_model_stat_collect,
+     0},
     {0}
 };
 
-static const st_trace_type  *nw_lp_get_trace_types(void)
+static const st_model_types  *nw_lp_get_model_stat_types(void)
 {
-    return(&nw_lp_trace_types[0]);
+    return(&nw_lp_model_types[0]);
 }
 
-void nw_lp_register_trace()
+void nw_lp_register_model()
 {
-    trace_type_register("nw-lp", nw_lp_get_trace_types());
+    st_model_type_register("nw-lp", nw_lp_get_model_stat_types());
 }
 /* end of ROSS event tracing setup */
 
@@ -1659,8 +1666,8 @@ int main( int argc, char** argv )
    nw_add_lp_type();
    model_net_register();
 
-    if (g_st_ev_trace)
-        nw_lp_register_trace();
+    if (g_st_ev_trace || g_st_model_stats)
+        nw_lp_register_model();
 
    net_ids = model_net_configure(&num_nets);
 //   assert(num_nets == 1);

src/network-workloads/model-net-synthetic-fattree.c
@@ -130,22 +130,29 @@ void ft_svr_event_collect(svr_msg *m, tw_lp *lp, char *buffer, int *collect_flag
     memcpy(buffer, &type, sizeof(type));
 }
 
-st_trace_type ft_svr_trace_types[] = {
+void ft_svr_model_stat_collect(svr_state *s, tw_lp *lp, char *buffer)
+{
+    return;
+}
+
+st_model_types ft_svr_model_types[] = {
     {(rbev_trace_f) ft_svr_event_collect,
      sizeof(int),
      (ev_trace_f) ft_svr_event_collect,
-     sizeof(int)},
+     sizeof(int),
+     (model_stat_f) ft_svr_model_stat_collect,
+     0},
     {0}
 };
 
-static const st_trace_type  *ft_svr_get_trace_types(void)
+static const st_model_types  *ft_svr_get_model_stat_types(void)
 {
-    return(&ft_svr_trace_types[0]);
+    return(&ft_svr_model_types[0]);
 }
 
-void ft_svr_register_trace()
+void ft_svr_register_model_stats()
 {
-    trace_type_register("server", ft_svr_get_trace_types());
+    st_model_type_register("server", ft_svr_get_model_stat_types());
 }
 
 const tw_optdef app_opt [] =
@@ -437,7 +444,7 @@ int main(
     svr_add_lp_type();
 
     if (g_st_ev_trace)
-        ft_svr_register_trace();
+        ft_svr_register_model_stats();
 
     codes_mapping_setup();
 

src/network-workloads/model-net-synthetic.c
@@ -116,22 +116,29 @@ void svr_event_collect(svr_msg *m, tw_lp *lp, char *buffer, int *collect_flag)
     memcpy(buffer, &type, sizeof(type));
 }
 
-st_trace_type svr_trace_types[] = {
+void svr_model_stat_collect(svr_state *s, tw_lp *lp, char *buffer)
+{
+    return;
+}
+
+st_model_types svr_model_types[] = {
     {(rbev_trace_f) svr_event_collect,
      sizeof(int),
      (ev_trace_f) svr_event_collect,
-     sizeof(int)},
+     sizeof(int),
+     (model_stat_f) svr_model_stat_collect,
+     0},
     {0}
 };
 
-static const st_trace_type  *svr_get_trace_types(void)
+static const st_model_types  *svr_get_model_stat_types(void)
 {
-    return(&svr_trace_types[0]);
+    return(&svr_model_types[0]);
 }
 
-void svr_register_trace()
+void svr_register_model_types()
 {
-    trace_type_register("server", svr_get_trace_types());
+    st_model_type_register("server", svr_get_model_stat_types());
 }
 
 const tw_optdef app_opt [] =
@@ -393,8 +400,8 @@ int main(
     model_net_register();
     svr_add_lp_type();
 
-    if (g_st_ev_trace)
-        svr_register_trace();
+    if (g_st_ev_trace || g_st_model_stats)
+        svr_register_model_types();
 
     codes_mapping_setup();
 

commit 921d3e486f04520d4bb19b970e1a0aaea49b24ef

Fix bug introduced in 77e85ef978ec40a17d239e0ab52b222e075fee09
Change-Id: I430c19d46be11f00c4123118334f7e68f835a846

src/networks/model-net/fattree.c
@@ -1854,7 +1854,9 @@ void switch_packet_receive_rc(switch_state * s,
 	s_arrive_r++;
 #endif
     int output_port = msg->saved_vc;
-    tw_rand_reverse_unif(lp->rng);
+    if(s->params->routing != STATIC) {
+      tw_rand_reverse_unif(lp->rng);
+    }
     if(bf->c1)
     {
         tw_rand_reverse_unif(lp->rng);