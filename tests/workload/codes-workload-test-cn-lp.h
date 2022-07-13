8020dfdaa55f0e52bf98928f2b6ef3974d837eb1
Neil McGlohon <nmcglohon@gmail.com>
2022-07-13 18:46:24 -0400

    Fix lack of extern usage in test header

--- tests/workload/codes-workload-test-cn-lp.h
+++ tests/workload/codes-workload-test-cn-lp.h
@@ -11,8 +11,8 @@
 #include "codes/codes-workload.h"
 
 extern tw_lptype client_lp;
-char workload_type[MAX_NAME_LENGTH_WKLD];
-struct iolang_params ioparams;
+extern char workload_type[MAX_NAME_LENGTH_WKLD];
+extern struct iolang_params ioparams;
 
 void cn_op_complete(tw_lp *lp, tw_stime svc_time, tw_lpid gid);
 void cn_op_complete_rc(tw_lp *lp);
